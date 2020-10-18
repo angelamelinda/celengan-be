d1c950c2f18876b7381b93fb0715071a6e28b59e
Angela Melinda <angela.melinda@gmail.com>
2020-10-19 02:42:06 +0700

    Add middleware

--- src/helpers/middleware.ts
+++ src/helpers/middleware.ts
@@ -1,22 +1,38 @@
 import { VerifyErrors } from "jsonwebtoken";
-import { verifyToken } from "./helper"
+import { verifyToken } from "./helper";
 
 export const authenticate = async (req: any, res: any, next: any) => {
-    const token = (req.headers["x-access-token"] || req.headers["authorization"]).replace('Bearer ', '');
-    const callback = (err: VerifyErrors | null, decoded: any) => {
-        const currentDate = new Date().getTime();
-        const expiredDate = new Date(decoded.exp).getTime();
+  let token: string =
+    req.headers["x-access-token"] || req.headers["authorization"];
 
-        console.log(new Date(currentDate), new Date(expiredDate), new Date(decoded.exp));
-        if (err || decoded && currentDate > expiredDate) {
-            res.status(401).json({
-                message: 'unauthorized',
-            })
-        } else {
-            req.user = decoded.data
-            next();
-        }
+  if (!token || token == undefined) {
+    res.status(401).json({
+      message: "unauthorized",
+    });
+    next({
+      message: "no valid token",
+      name: "missing token",
+    });
+    return;
+  }
+
+  token = token.replace("Bearer ", "");
+  await verifyToken(token, (err: VerifyErrors | null, decoded: any) => {
+    const currentDate = new Date().getTime();
+    const expiredDate = new Date(decoded.exp).getTime();
+
+    if (err || (decoded && currentDate > expiredDate)) {
+      res.status(401).json({
+        message: "unauthorized",
+      });
+      next({
+        message: "invalid token",
+        name: "invalid token",
+      });
+      return;
+    } else {
+      req.user = decoded.data;
+      next();
     }
-
-    await verifyToken(token, callback)
-}
+  });
+};
